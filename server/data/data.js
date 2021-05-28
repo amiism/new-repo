572e8bb02e2da7451c899273594df9e91b4a914a
t-l-xin <e0556433@u.nus.edu>
2021-05-28 23:26:25 +0800

    update everything

---
+++ server/data/data.js
@@ -0,0 +1,12 @@
+const data = [
+    {
+        channelId: "00001",
+        channelName: "MA1521"
+    },
+    {
+        channelId: "00002",
+        channelName: "CS2100"
+    }
+];
+
+export default data;
