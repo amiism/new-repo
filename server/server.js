1dcf5e393a7de0bd192de7f074f9ac5db20d5c30
t-l-xin <e0556433@u.nus.edu>
2021-06-05 12:59:43 +0800

    test commit

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
-
 const books = require('./routes/api/books.js');
+require('dotenv').config();
+const path = require('path');
 
 const app = express();
+const port = (process.env.PORT || 5000);
 
 // Connect Database
 connectDB();
@@ -15,11 +17,23 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+//app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
+//to load the react app when upload to heroku
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/client/build')));
+    
+    app.get('*', (req,res)=>{
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
+}else{
+    app.get('/', (req,res)=>{
+        res.send('APi running');
+    });
+}
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
